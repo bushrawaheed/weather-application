00384c244d90ab01c91a035dc4d1c6f43a8733ad
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-02-23 13:27:06 +0100

    updating app css

--- src/final.js
+++ src/final.js
@@ -44,7 +44,7 @@ function displayForecast(response){
     forecastElement.innerHTML=null;
     let forecast=null;
     
-    for (let index = 0;index<12;index++){
+    for (let index = 0;index<6;index++){
         forecast=response.data.list[index];
         forecastElement.innerHTML +=`
         <div class="col-2">
